13107b8a275793c8d5b1be90175ed2e2216f874b
Hang Chan <hang@dstillery.com>
2018-07-26 22:20:58 -0400

    remove whitespace

--- vowels4.py
+++ vowels4.py
@@ -11,5 +11,5 @@ for letter in word:
     if letter in vowels:
         found[letter] += 1
 
-for k,v in sorted( found.items() ):
-    print( k, 'was found', v, 'time(s).' )
+for k,v in sorted(found.items()):
+    print(k, 'was found', v, 'time(s).')
